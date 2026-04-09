8f4454582864b2c813e36ab3d55217a318b6121c
DoreAupp <2024553te@aupp.edu.kh>
2026-04-09 15:20:47 +0700

    Final setup: CI/CD with GitHub Actions + Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "foodexpress_sg" {
+  name        = "foodexpress-sg"
+  description = "Allow SSH and app port"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "API Port"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "foodexpress_ec2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.foodexpress_sg.id]
+
+  tags = {
+    Name = "foodexpress-api-server"
+  }
+}
